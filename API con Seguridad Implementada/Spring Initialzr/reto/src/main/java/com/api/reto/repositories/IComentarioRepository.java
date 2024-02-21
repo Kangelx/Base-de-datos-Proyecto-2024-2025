54869405e9299cf8a996f40638cf285f0e82f5da
Pablo Sainz Luque <DAM225@iesmhp.local>
2024-02-21 19:55:04 +0100

    Comenzando la documentación

--- API con Seguridad Implementada/Spring Initialzr/reto/src/main/java/com/api/reto/repositories/IComentarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.api.reto.repositories;
-
-import com.api.reto.models.ComentariosEntity;
-import com.api.reto.models.EquiposEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IComentarioRepository extends JpaRepository<ComentariosEntity, Integer> {
-
-}
--- API con Seguridad Implementada/Spring Initialzr/reto/src/main/java/com/api/reto/repositories/IPersonalRepository.java
+++
@@ -1,8 +0,0 @@
-package com.api.reto.repositories;
-
-import com.api.reto.models.IncidenciasEntity;
-import com.api.reto.models.PersonalEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IPersonalRepository extends JpaRepository<PersonalEntity, Integer> {
-}
